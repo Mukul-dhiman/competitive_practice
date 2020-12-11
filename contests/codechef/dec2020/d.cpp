0c51b6f5947e76b91f017d1937d90bb44983ad70
Mukul-dhiman <orton1615@gmail.com>
2020-12-11 09:18:24 +0530

    dec2020 q4

--- contests/codechef/dec2020/d.cpp
+++ contests/codechef/dec2020/d.cpp
@@ -14,8 +14,95 @@ void pre(){
     //code for pre celculations
 
 }
+int n,k;
 void solve(){
-    
+     cin >> n >> k;
+    int tmp = 0;
+    vector<int> A(n+4);
+    loop(i,n-n%4){
+        if(tmp == k){
+            A[i] = -i-1;
+        } 
+        else if(n-i+tmp == k){
+            A[i] = i+1;
+            tmp +=1;
+        }
+        else if(tmp+n-i-3>=k){
+            A[i] = i+1;
+            A[i+1] = -(i+2);
+            A[i+2] = -i-3;
+            A[i+3] = i+4;
+            i +=3;
+            tmp +=1;
+        }
+        else if(tmp+n-i-3 == k - 1){
+            A[i] = -i-1;
+            A[i+1] = i+2;
+            A[i+2] = i+3;
+            A[i+3] = -i-4;
+            i +=3;
+            tmp +=2;
+        }
+        else if(tmp+n-i-3 == k-2){
+            A[i] = i+1;
+            A[i+1] = -i-2;
+            A[i+2] = i+3;
+            A[i+3] = i+4;
+            i +=3;
+            tmp +=3;
+        }
+          
+    }
+    int sw = n%4;
+    switch (sw){
+        case 1:
+            if(k != tmp) A[n-1] = n;
+            else
+            {
+                A[n-1] = -n;
+            }
+            break;
+        case 2:
+            if( k == tmp){
+                A[n-2] = -n+1;
+                A[n-1] = -n;
+            }
+            else if(k == tmp+1){
+                A[n-2] = -n+1;
+                A[n-1] = n;
+            }
+            else{
+                A[n-2] = n-1;
+                A[n-1] = n;
+            }
+            break;
+        case 3:
+            if(k == tmp){
+                A[n-3] = -n+2;
+                A[n-2] = -n+1;
+                A[n-1] = -n;
+            }
+            else if(k == tmp+1){
+                A[n-3] = n-2;
+                A[n-2] = -n+1;
+                A[n-1] = -n;
+            }
+            else if(k == tmp+2){
+                A[n-3] = n-2;
+                A[n-2] = -n+1;
+                A[n-1] = n;
+            }
+            else{
+                A[n-3] = n-2;
+                A[n-2] = n-1;
+                A[n-1] = n;
+            }
+            break;
+    }
+    loop(i,n){
+        cout<<A[i]<<' ';
+    }
+    cout<<endl;
     return; 
 }
 void test(){
