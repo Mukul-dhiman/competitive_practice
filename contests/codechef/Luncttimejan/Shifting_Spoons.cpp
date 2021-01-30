1dc1dcd79b6962912940ebaeb2f7052cc4ccbe9c
Mukul-dhiman <orton1615@gmail.com>
2021-01-30 23:54:12 +0530

    lunchtime jan

--- contests/codechef/Luncttimejan/Shifting_Spoons.cpp
+++ contests/codechef/Luncttimejan/Shifting_Spoons.cpp
@@ -15,33 +15,66 @@ void pre(){
     //code for pre celculations
 
 }
+struct Test{ 
+   int x, y;
+   ll z; 
+};
 void solve(){
     int n;
     cin>>n;
     ll f,temp;
     cin>>f;
     priority_queue<pi, vector<pi>, greater<pi> > pq; 
-    vector<pi> ans;
+    vector<Test> ans;
     pi t;
     iloop(2,i,n){
         cin>>temp;
         pq.push(make_pair(temp,i));
     }
+    int count=0;
+    Test next;
     while(!pq.empty()){
+        count++;
         t = pq.top();
         pq.pop();
         if(f>=t.first){
-            ans.pb(t);
+            next.x=t.second;
+            next.y=1;
+            next.z=t.first;
+            ans.pb(next);
             f+=t.first;
         }
         else{
+            if(pq.empty()){
+                count=2*n;
+            }
+            else{
+                pi nt = pq.top();
+                pq.pop();
+                ll dif = t.first - f;
+                t.first -= dif;
+                nt.first += dif;
+                f+=t.first;
+                next.x=t.second;
+                next.y=nt.second;
+                next.z=dif;
+                ans.pb(next);
+
+                next.x=t.second;
+                next.y=1;
+                next.z=t.first;
+                ans.pb(next);
+                pq.push(nt);
+            }
+        }
+        if(count>=2*n){
             out("-1");
             return;
         }
     }
     out(ans.size());
-    for(auto x:ans){
-        cout<<x.second<<" "<<"1 "<<x.first<<endl;
+    for(auto l:ans){
+        cout<<l.x<<" "<<l.y<<" "<<l.z<<endl;
     }
     return; 
 }
